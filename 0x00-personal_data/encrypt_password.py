c7be80ee48b50bd981a592855012846522ae6000
Xolelwa34 <ciliaswekile@gmail.com>
2024-11-07 15:56:02 +0000

    adding passwd

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""main bycript"""
+
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hash a password with a salt using bcrypt.
+    Returns a hashed password in byte form.
+    """
+    # Generate a salt
+    salt = bcrypt.gensalt()
+    
+    # Hash the password using the salt
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    
+    return hashed_password
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Check if a provided password matches the hashed password.
+    Returns True if they match, False otherwise.
+    """
+    # Check if the password matches the hashed password
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
