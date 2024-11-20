ce03239090e104f8611e38ab1d7a9c95d760532d
Xolelwa34 <ciliaswekile@gmail.com>
2024-11-20 12:12:47 +0000

    User

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module for users model
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    """
+    User method object
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
